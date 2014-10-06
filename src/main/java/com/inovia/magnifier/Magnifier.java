c13bf47a8a0fb41a77be75be26c7f57bd8314f6c
joeyrogues <rogues.jonathan.168@gmail.com>
2014-10-06 09:49:10 -0700

    #35764 @0.12 Refactored and commented all methods

--- src/main/java/com/inovia/magnifier/Magnifier.java
+++ src/main/java/com/inovia/magnifier/Magnifier.java
@@ -1,15 +1,18 @@
 package com.inovia.magnifier;
 
-import com.inovia.magnifier.databaseObjects.Index;
-import com.inovia.magnifier.databaseObjects.Table;
+import com.inovia.magnifier.databaseObjects.*;
 
-
+/**
+ * 
+ * @author joeyrogues
+ *
+ */
 public class Magnifier {
 	public static void main(String[] args) {
 		Configuration conf = null;
 		Database database = null;
 		try { // This is for testing
-			
+
 			conf = new Configuration(args);
 			database = new Database(conf);
 
@@ -18,7 +21,7 @@ public class Magnifier {
 				System.out.println(t);
 			}
 			System.out.println();
-			/*
+
 			System.out.println("Indexes");
 			for(Index i : database.getIndexes()) {
 				System.out.println(i);
@@ -26,7 +29,7 @@ public class Magnifier {
 			System.out.println();
 
 			System.out.println("Alright");
-			*/
+
 		} catch(UnsupportedOperationException e) {
 			e.printStackTrace();
 			System.exit(1);
