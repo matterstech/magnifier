b25230e1086ddf9be15186a63127ac31f0c28dd8
joeyrogues <rogues.jonathan.168@gmail.com>
2014-10-15 13:56:05 -0700

    refactored the report

--- src/main/java/com/inovia/magnifier/reports/Report.java
+++ src/main/java/com/inovia/magnifier/reports/Report.java
@@ -28,81 +28,83 @@ public class Report {
 		PrintWriter writer = null;
 		try {
 			writer = new PrintWriter(path, "UTF-8");
-
-			writer.println("<html>");
-			writer.println("  <head>");
-			writer.println("    <style>");
-			writer.println("      table {");
-			writer.println("        width: 100%;");
-			writer.println("        text-align: left;");
-			writer.println("        border: 1px black ridge");
-			writer.println("      }");
-			writer.println("    </style>");
-			writer.println("  </head>");
-			writer.println("  <body>");
-			writer.println("  <h1>" + databaseName + "</h1>");
-			writer.println("    <table>");
-			writer.println("      <thead>");
-			writer.println("        <tr>");
-			writer.println("          <th>Rule</th>");
-			writer.println("          <th>Score</th>");
-			writer.println("          <th>Suggestions</th>");
-			writer.println("          <th>Technical debt</th>");
-			writer.println("          <th>Entity</th>");
-			writer.println("        </tr>");
-			writer.println("      </thead>");
-			writer.println("      <tbody>");
+			String html = "";
+			html = html + "<html>"
+						+ "  <head>"
+						+ "    <style>"
+						+ "      table {"
+						+ "        width: 100%;"
+						+ "        text-align: left;"
+						+ "        border: 1px black ridge"
+						+ "      }"
+						+ "    </style>"
+						+ "  </head>"
+						+ "  <body>"
+						+ "    <h1>" + databaseName + "</h1>"
+						+ "    <table>"
+						+ "      <thead>"
+						+ "        <tr>"
+						+ "        <th></th>"
+						+ "        <th>Rule</th>"
+						+ "        <th>Score</th>"
+						+ "        <th>Suggestions</th>"
+						+ "        <th>Technical debt</th>"
+						+ "        <th>Entity</th>"
+						+ "      </tr>"
+						+ "    </thead>"
+						+ "  <tbody>";
 			for(RuleReport rr : ruleReports) {
-				writer.println("        <tr id=\"" + rr.getRuleName() + "-plus\">");
+				html = html + "<tr id=\"" + rr.getRuleName() + "-plus\">";
 				if(rr.getScore() < 100F) {
-					writer.println("        <td><button class=\"fold-button\" id=\"" + rr.getRuleName() + "\">" + rr.getRuleName() + "</button></td>");
+					html = html + "<td><button class=\"fold-button\" id=\"" + rr.getRuleName() + "\"> +/- </button></td>";
 				} else {
-				    writer.println("          <td>" + rr.getRuleName() + "</td>");
+				    html = html + "<td></td>";
 				}
-				writer.println("          <td>" + rr.getScore().intValue() + "%</td>");
-				writer.println("          <td>" + rr.getSuggestion() + "</td>");
-				writer.println("          <td>" + rr.getDebt() + "</td>");
-				writer.println("          <td>" + "" + "</td>");
-				writer.println("        </tr>");
-				writer.println("        <tr id=\"" + rr.getRuleName() + "-plus\">");
-				writer.println("          <td colspan=\"5\">");
+				html = html + "  <td>" + rr.getRuleName() + "</td>"
+						    + "  <td>" + rr.getScore().intValue() + "%</td>"
+							+ "  <td>" + rr.getSuggestion() + "</td>"
+							+ "  <td>" + rr.getDebt() + "</td>"
+							+ "  <td>" + "" + "</td>"
+							+ "</tr>"
+							+ "<tr id=\"" + rr.getRuleName() + "-plus\">"
+							+ "  <td colspan=\"5\">";
 				if(rr.getScore() < 100F) {
-					writer.println("            <table id=\"" + rr.getRuleName() + "-table\" style=\"display: none\">");
-					writer.println("              <tbody>");
+					html = html + "  <table id=\"" + rr.getRuleName() + "-table\" style=\"display: none\">"
+							    + "<tbody>";
 				}
 				for(ReportEntry e : rr.getEntries()) {
 					if(!e.isSuccess()) {
-						writer.println("            <tr data-rule=\"" + rr.getRuleName() + "\">");
-						writer.println("              <td>" + e.getEntityDescription() + "</td>");
-						writer.println("            </tr>");
+						html = html + "<tr data-rule=\"" + rr.getRuleName() + "\">"
+								    + "  <td>" + e.getEntityDescription() + "</td>"
+								    + "</tr>";
 					}
 				}
 				if(rr.getScore() < 100F) {
-					writer.println("              </tbody>");
-					writer.println("            </table>");
+					html = html + "</tbody>"
+					            + "  </table>";
 				}
-				writer.println("          </td>");
-				writer.println("        </tr>");
+				html = html + "  </td>"
+				        	+ "</tr>";
 			}
-			writer.println("      </tbody>");
-			writer.println("    </table>");
-
-			writer.println("    <script>");
-			writer.println(       "var t = document.getElementsByClassName('fold-button');");
-			writer.println(       "for(var i = t.length-1 ; i >= 0 ; i--) {");
-			writer.println("       t[i].addEventListener('click', function(e) {");
-			writer.println("       element_style = document.getElementById(e.target.id + '-table').style;");
-			writer.println("         if(element_style.display != 'none') {");
-			writer.println("           element_style.display = 'none'");
-			writer.println("         } else if(element_style.display == 'none') {");
-			writer.println("           element_style.display = ''");
-			writer.println("         }");
-			writer.println("       });");
-			writer.println("}");
-		writer.println("    </script>");
-
-		writer.println("  </body>");
-		writer.println("</html>");
+			html = html + "      </tbody>"
+				        + "    </table>"
+				        + "    <script>"
+				        + "      var t = document.getElementsByClassName('fold-button');"
+				        + "      for(var i = t.length-1 ; i >= 0 ; i--) {"
+				        + "        t[i].addEventListener('click', function(e) {"
+				        + "          element_style = document.getElementById(e.target.id + '-table').style;"
+				        + "          if(element_style.display != 'none') {"
+				        + "            element_style.display = 'none'"
+				        + "          } else if(element_style.display == 'none') {"
+				        + "            element_style.display = ''"
+				        + "          }"
+				        + "        });"
+				        + "      }"
+				        + "    </script>"
+				        + "  </body>"
+				        + "<html>";
+		
+		writer.println(html);
 	} catch (FileNotFoundException e) {
 		e.printStackTrace();
 	} catch (UnsupportedEncodingException e) {
